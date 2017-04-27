660a1273dce454bf397ce055fa6956cf18e85d9d
Silvio Kennecke <development@silvio-kennecke.de>
2017-04-27 21:21:48 +0200

    Berücksichtigt nun auch Klassen

--- check-required-extensions.php
+++ check-required-extensions.php
@@ -30,7 +30,7 @@ foreach ($files as $key => $file) {
 // begin sanning
 
 // remove everything else
-$functions = ['methods' => [], 'functions' => []];
+$functions = ['functions' => [], 'classes' => []];
 foreach ($files as $file) {
 	$content = file_get_contents($file);
 	$content = preg_replace('/<\?=[\w\W]*\?>/imU', '', $content);
@@ -45,7 +45,7 @@ foreach ($files as $file) {
 		$content = substr($content, 0, strpos($content, '?>') * (-1));
 	}
 	
-	if (preg_match_all('/(?<is_method>(::|->)?)\W*(?<function>[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)\W*\(/m', $content, $matches, PREG_SET_ORDER)) {
+	if (preg_match_all('/(?<is_method>(([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)::|->)?)\W*(?<function>[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)\W*\(/m', $content, $matches, PREG_SET_ORDER)) {
 		foreach($matches as $match) {
 			if (empty($match['is_method'])) {
 				if (!key_exists($match['function'], $functions['functions'])) {
@@ -54,24 +54,67 @@ foreach ($files as $file) {
 				if (!in_array($file, $functions['functions'][$match['function']])) {
 					$functions['functions'][$match['function']][] = $file;
 				}
-			} else {
-				if (!key_exists($match['function'], $functions['methods'])) {
-					$functions['methods'][$match['function']] = [];
+			} else if ($match['is_method'] != '->') {
+				$class_name = substr($match['is_method'], 0, -2);
+				
+				if (!key_exists($class_name, $functions['classes'])) {
+					$functions['classes'][$class_name] = [];
 				}
-				if (!in_array($file, $functions['methods'][$match['function']])) {
-					$functions['methods'][$match['function']][] = $file;
+				if (!in_array($file, $functions['classes'][$class_name])) {
+					$functions['classes'][$class_name][] = $file;
 				}
+			}
+		}
+	}
+	if (preg_match_all('/new\W+(?<class>[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)/m', $content, $matches, PREG_SET_ORDER)) {
+		foreach($matches as $match) {
+			if (!key_exists($match['class'], $functions['classes'])) {
+				$functions['classes'][$match['class']] = [];
+			}
+			if (!in_array($file, $functions['classes'][$match['class']])) {
+				$functions['classes'][$match['class']][] = $file;
 			}
 		}
 	}
 }
 
 // get extensions and functions
-$extensions = get_loaded_extensions();
-foreach ($extensions as $key => $name) {
-	$extensions[$name] = get_extension_funcs($name);
-	if (!$extensions[$name]) {
-		unset($extensions[$name]);
+function getExtensionVars ($name) {
+	$ext = new ReflectionExtension($name);
+	
+	$extensions = [];
+	
+	$extensions['functions'] = $ext->getFunctions();
+	if (!$extensions['functions']) {
+		$extensions['functions'] = [];
+	} else {
+		$extensions['functions'] = array_keys($extensions['functions']);
+	}
+	
+	$extensions['classes'] = $ext->getClassNames();
+	if (!$extensions['classes']) {
+		$extensions['classes'] = [];
+	} else {
+		$extensions['classes'] = array_keys($extensions['classes']);
+	}
+	
+	$extensions['constants'] = $ext->getClassNames();
+	if (!$extensions['constants']) {
+		$extensions['constants'] = [];
+	} else {
+		$extensions['constants'] = array_keys($extensions['constants']);
+	}
+	
+	return $extensions;
+}
+
+$extensions_list = get_loaded_extensions();
+$extensions = [];
+foreach ($extensions_list as $key => $name) {
+	$ext = new ReflectionExtension($name);
+	
+	if (!key_exists($name, $extensions)) {
+		$extensions[$name] = getExtensionVars($name);
 	}
 	
 	unset($extensions[$key]);
@@ -81,23 +124,23 @@ foreach ($extensions as $key => $name) {
 $required = [];
 foreach ($functions['functions'] as $function => $files) {
 	foreach($extensions as $name => $funcs) {
-		if (in_array($function, $funcs)) {
+		if (in_array($function, $funcs['functions'])) {
 			if (!array_key_exists($name, $required)) {
-				$required[$name] = [];
+				$required[$name] = ['functions' => [], 'classes' => []];
 			}
 			
-			$required[$name][$function] = $files;
+			$required[$name]['functions'][$function] = $files;
 		}
 	}
 }
-foreach ($functions['methods'] as $function => $files) {
-	foreach($extensions as $name => $funcs) {
-		if (in_array($function, $funcs)) {
+foreach ($functions['classes'] as $function => $files) {
+	foreach($extensions as $name => $funcs['classes']) {
+		if (in_array($function, $funcs['classes'])) {
 			if (!array_key_exists($name, $required)) {
-				$required[$name] = [];
+				$required[$name] = ['functions' => [], 'classes' => []];
 			}
 			
-			$required[$name][$function] = $files;
+			$required[$name]['classes'][$function] = $files;
 		}
 	}
 }
