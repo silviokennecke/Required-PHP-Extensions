1e406c8dceac876235a458e35331c9532c8622d4
Silvio Kennecke <development@silvio-kennecke.de>
2017-05-05 14:37:49 +0200

    Added CLI-Colors

--- client-check.php
+++ client-check.php
@@ -1,22 +1,22 @@
 <?php
 
 if (!function_exists('json_decode')) {
-	die('Please install php-json!'."\r\n");
+	die("\033[0;31mPlease install php-json!\r\n\033[0m");
 }
 
 if (!isset($argv) || count($argv) < 1) {
-	die('Please hand over a requirements json file!'."\r\n");
+	die("\033[0;31mPlease hand over a requirements json file!\r\n\033[0m");
 }
 
 if (!file_exists($argv[1]) || !is_readable($argv[1])) {
-	die('Please hand over a readable file!'."\r\n");
+	die("\033[0;31mPlease hand over a readable file!\r\n\033[0m");
 }
 
 $contents = file_get_contents($argv[1]);
 $extensions = json_decode($contents, true);
 
 if (!$extensions) {
-	die('The requirements json file does not contain valid JSON: ' . json_last_error_msg() . "\r\n");
+	die("\033[0;31mThe requirements json file does not contain valid JSON: " . json_last_error_msg() . "\r\n\033[0m");
 }
 
 $loaded_extensions = get_loaded_extensions();
@@ -28,9 +28,9 @@ foreach ($extensions as $extension => $needed_by) {
 }
 
 if (empty($errors)) {
-	echo 'All required extensions are installed correctly!'."\r\n";
+	echo "\033[0;32mAll required extensions are installed correctly!\r\n\033[0m";
 } else {
-	echo 'The followind extensions are not (fully) installed or missing:'."\r\n";
+	echo "\033[0;32mThe followind extensions are not (fully) installed or missing:\r\n\033[0m";
 	foreach ($errors as $extension => $error) {
 		echo $extension . "\r\n";
 	}
